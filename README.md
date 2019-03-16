db04a48c62c722a24352c0e62754eb2c2441d935
Elinor Wahl <shamziel@gmail.com>
2019-03-15 20:43:54 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,88 @@
+# Adversarial Search Game-Playing Agent
+
+### Synopsis
+
+In this project, I have built two agents that use adversarial search techniques - minimax with alpha-beta search pruning, and Monte Carlo tree search - to play knight's Isolation. This is adapted from Udacity's Artificial Intelligence course materials, which can be found [here](https://github.com/udacity/artificial-intelligence/tree/master/Projects/3_Adversarial%20Search).
+
+![Example game of isolation on a square board](viz.gif)
+
+In the game Isolation, two players each control one token, and take turns moving their token from one cell to another on a rectangular grid. In knight's Isolation, tokens can move to any open cell that is 2-rows-and-1-column or 2-columns-and-1-row away from their current position on the board, in an 'L' shape. Whenever a token occupies a cell, that cell is blocked for the remainder of the game. On a blank board, this means that tokens have, at most, eight open cells to move into surrounding their current location. 'Knight' tokens can 'jump' blocked or occupied spaces (just like a knight in chess). The first player with no remaining open moves, or 'liberties' for their token loses the game, and their opponent is declared the winner.
+
+In this implementation, agents have a fixed time limit (150 milliseconds by default) to search for the best move and respond. The search is automatically cut off after the time limit expires, and the active agent will forfeit the game if it has not chosen a move.
+
+### Search Techniques
+
+`sample_players.py` contains three players to use as opponents - 'Random', 'Greedy', and 'Minimax'. In `my_custom_player.py`, I have implemented two more advanced agents: Minimax with alpha-beta search pruning, and Monte Carlo tree search (MCTS) with upper confidence trees. This is how each of them function:
+
+- `RandomPlayer` randomly selects a move from the moves available to its position on the board at each turn.
+
+- `GreedyPlayer` selects a move projected to maximize its own score at each turn.
+
+- `MinimaxPlayer` functions by 'maximizing' the 'minimum' available reward - it calculates the smallest possible reward that it can be forced into by another player's move, and the largest possible reward it can obtain when it knows the actions of the opposing player.
+
+- `MinimaxAlphaBetaPlayer` improves on the minimax algorithm by employing an 'alpha' value representing the minimum score that the maximizing player can obtain, and a 'beta' value representing the maximum score that the minimizing player can obtain.
+
+- `MonteCarloUCTPlayer` acts in four stages: 'Selection', in which it chooses a node and travels along a branch, 'expansion', in which it creates more nodes if a terminal state hasn't been reached and selects the most promising child node, 'simulation', in which it runs a simulated playout from that node and returns the final result, and 'backpropagation', in which it applies the result of the simulation moves to all the previous nodes along the current search tree. The 'upper confidence trees' function is provided by an equation within the selection function, which combines ‘exploration' of new paths with ‘exploitation' of paths that result in high scores for the player. Exploration is disabled outside of the simulation phase (the 'factor' that serves as multiplier for exploration is set to 0), so in actual gameplay the agent uses only the moves that it knows are likely to result in a victory.
+
+### Performance
+
+To test the Minimax Alpha-Beta Player and the Monte Carlo UCT Player, I used each to play 100 rounds of Isolation against the Random, Greedy, and Minimax players. This table represents the percentage of matches they won against each opponent:
+
+|| Against Random Agent | Against Greedy Agent | Against Minimax Agent |
+|:-:|:-:|:-:|:-:|
+| Minimax Alpha-Beta Agent | 95% | 86% | 69% |
+| Monte Carlo UCT Agent | 100% | 94% | 89% |
+
+These are both markedly better than even an agent playing by the minimax heuristic!
+
+- The minimax alpha-beta agent performs better than minimax because of increased efficiency, which makes it run faster. When, at a particular node, the maximum score that the 'beta' player can obtain becomes less than the minimum score the 'alpha' player can obtain, the children of this node are dropped from consideration while traveling the search tree, and the game moves they represent will no longer be attempted. This pruning of the search tree makes the agent's processes much faster and more efficient.
+
+- The Monte Carlo agent performs better than minimax because of increased calculation power, with fewer consequences on active gameplay. The exploit/explore equation provides a stronger mathematical basis for choosing potential moves than either minimax or minimax alpha-beta, and the simulated rollout function ensures that the agent can test the effectiveness of moves without endangering its position while the game is in progress. Then, backpropagation lets the the algorithm distribute the results backwards along the entire search path, further cementing the learned information about the value of potential moves.
+
+However - the Monte Carlo agent takes considerably longer to play matches than the minimax alpha-beta agent. The win rates recorded above only apply to the original version of the `monte_carlo_uct` function, which is this:
+
+```
+def monte_carlo_uct(self, state):
+    if 'TIME_LIMIT' in globals():
+        buffer = 50.
+        delta = (TIME_LIMIT-buffer) / 1000.
+    else:
+        delta = 0.1
+    timer_end  = time.time() + delta
+    root = Node(state)
+    while time.time() < timer_end:
+        leaf = self.tree_policy(root)
+		value = self.rollout_policy(leaf, state.player())
+        self.backprop(leaf, value)
+    result = root.children.index(self.best_child(root, 0))
+    return root.child_actions[result]
+```
+
+This agent, while extremely successful at Isolation, runs slowly enough that it sometimes can't make a move within the allotted time limit of 150 milliseconds. The version of the function that can meet the time limit is this:
+
+``` 
+def monte_carlo_uct(self, state):
+	root = Node(state)
+	# Return a random move in the event of a time-out
+	if root.state.terminal_test():
+		return random.choice(state.actions())
+	for i in range(1, 100):
+        leaf = self.tree_policy(root)
+	    # Return to the top of the search loop if no best child is found
+        if not leaf:
+			continue
+        value = self.rollout_policy(leaf, state.player())
+        self.backprop(leaf, value)
+    result = root.children.index(self.best_child(root, 0))
+    return root.child_actions[result]
+```
+
+This simple change makes the Monte Carlo tree search run much faster, but with a serious impact on performance. With this in effect, the Monte Carlo agent won 71% of 100 matches against the minimax agent. Furthermore, running it often resulted in a hung terminal. Where the Monte Carlo algorithm is concerned, there appears to be a very large trade-off between speed and performance.
+
+So how to fix this? One possible answer: Use NumPy instead of the built-in `math` package. While NumPy can't function with `isolation.py` for use in this project, it would make the program run much more quickly than the 'math' package by enabling it to perform calculations across entire vectors of numbers. This is critical because in order to be effective, Monte Carlo tree search must explore and test a large number of nodes.
+
+### References
+
+A very good, simple implementation of Monte Carlo tree search, which I used to build my own program, can be found on GitHub in [this Connect 4 player](https://github.com/Alfo5123/Connect4).
+
+Additionally, [this blog post on Monte Carlo UCT search](http://www.moderndescartes.com/essays/deep_dive_mcts/) and [the associated code](https://github.com/brilee/python_uct) are excellent explanations of how Monte Carlo tree search with an upper confidence heuristic works, and how to optimize its functioning.
---
+++ my_custom_player.py
@@ -0,0 +1,215 @@
+
+import math
+import random
+import time
+from sample_players import DataPlayer
+
+   
+"""
+    **********************************************************************
+                          Minimax Alpha-Beta Player
+    **********************************************************************
+"""
+
+class MinimaxAlphaBetaPlayer(DataPlayer):
+    """ Implement a player that combines minimax with alpha-beta pruning
+    to play knight's Isolation.
+    """
+    def get_action(self, state):
+        """ Randomly select a move as player 1 or 2 on an empty board, otherwise
+        return the optimal minimax move at a fixed search depth of 5 plies.
+        """
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            depth_limit = 5
+            for depth in range(1, depth_limit + 1):
+                self.queue.put(self.minimax_alpha_beta(state, depth))              
+
+    def minimax_alpha_beta(self, state, depth):
+        """ Return the move along a branch of the game tree that has the
+        best possible value.
+        """
+
+        def min_value(state, alpha, beta, depth):
+            """ Return the value for a win (+1) if the game is over,
+            otherwise return the minimum value over all legal child nodes.
+            """
+            if state.terminal_test():
+                return state.utility(self.player_id)
+            if depth <= 0:
+                return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), alpha, beta, depth - 1))
+                if value <= alpha:
+                    return value
+                beta = min(beta, value)
+            return value
+
+        def max_value(state, alpha, beta, depth):
+            """ Return the value for a loss (-1) if the game is over,
+            otherwise return the maximum value over all legal child nodes.
+            """
+            if state.terminal_test():
+                return state.utility(self.player_id)
+            if depth <= 0:
+                return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), alpha, beta, depth - 1))
+                if value >= beta:
+                    return value
+                alpha = max(alpha, value)
+            return value
+
+        return max(state.actions(), key=lambda x: min_value(state.result(x), float("-inf"), 
+                                                            float("inf"), depth - 1))
+
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        return len(own_liberties) - len(opp_liberties)
+
+"""
+    **********************************************************************
+              Monte Carlo Upper Confidence Tree Search Player
+    **********************************************************************
+"""
+
+class Node():
+    """ Implement a class that saves the states of game move values,
+    child nodes, parent nodes, and expansions along the search tree.
+
+    Adapted from the Connect 4 player here:
+    https://github.com/Alfo5123/Connect4/blob/master/game.py
+    """
+    def __init__(self, state, parent=None):
+        self.visits = 1
+        self.value = 0.0
+        self.state = state
+        self.children = []
+        self.child_actions = []
+        self.parent = parent
+
+    def add_child(self, child_state, action):
+        child = Node(child_state, self)
+        self.children.append(child)
+        self.child_actions.append(action)
+
+    def update(self, value):
+        self.value += value
+        self.visits += 1
+
+    def fully_explored(self):
+        if len(self.child_actions) == len(self.state.actions()):
+            return True
+        return False
+        
+
+class MonteCarloUCTPlayer(DataPlayer):
+    """ Implement a player that invokes the Node class and combines
+    Monte Carlo tree search with an upper confidence tree heuristic
+    to play knight's Isolation.
+
+    Adapted from the Connect 4 player here:
+    https://github.com/Alfo5123/Connect4/blob/master/game.py
+    """
+    def get_action(self, state):
+        """ Randomly select a move as player 1 or 2 on an empty board, 
+        otherwise return the optimal Monte Carlo move.
+        """
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            self.queue.put(self.monte_carlo_uct(state))
+    
+    def expand(self, node):
+        """ Select a legal move and advance to the corresponding child node.
+        """
+        possible_actions = node.state.actions()
+        child_action_attempts = [action for action in node.child_actions]
+        for action in possible_actions:
+            if action not in child_action_attempts:
+                new_state = node.state.result(action)
+                break
+        node.add_child(new_state, action)
+        return node.children[-1]
+
+    def exploit(self, node):
+        return node.value / node.visits
+
+    def explore(self, node):
+        return math.sqrt(2.0 * math.log(node.parent.visits) / node.visits)
+
+    def best_child(self, node, factor = 1.0):
+        """ Use upper confidence tree search to select a child node
+        that returns the best scores.
+	"""
+        return max(node.children, key=lambda child: self.exploit(child) + factor * self.explore(child))
+    
+    def tree_policy(self, node):
+        """ Implement expansion of the search tree. Return an unexplored node
+        if the branch isn't fully explored, and the best child node if it is.
+        """
+        while not node.state.terminal_test():
+            if not node.fully_explored():
+                return self.expand(node)
+            node = self.best_child(node)
+        return node
+        
+    def rollout_policy(self, node, player):
+        """ Copy the state of the latest expanded node, randomly choose 
+        a legal move, play the game to a terminal state, and record a winner.
+        """
+        state = node.state
+        while not state.terminal_test():
+            action = random.choice(state.actions())
+            state = state.result(action)
+        if state._has_liberties(node.state.player()):
+            return -1
+        else: 
+            return 1
+    
+    def backprop(self, node, value):
+        """ Store the reward values of all the visited nodes while traveling
+        backwards along the search tree. Flip the values between player 1 
+        and player 2, to let the player tell its own moves from the opponent's.
+        """
+        while node:
+            node.update(value)
+            node = node.parent
+            value = -value
+    
+    def monte_carlo_uct(self, state):
+        """ Locate and backpropagate nodes along the search tree, corresponding 
+        to moves which frequently lead to wins in rollout simulation games.
+        """
+        # if 'TIME_LIMIT' in globals():
+            # buffer = 50.
+            # delta = (TIME_LIMIT-buffer) / 1000.
+        # else:
+            # delta = 0.1
+        # timer_end  = time.time() + delta
+        root = Node(state)
+        if root.state.terminal_test():
+            return random.choice(state.actions())
+        # while time.time() < timer_end:
+        for i in range(1, 100):
+            leaf = self.tree_policy(root)
+            if not leaf:
+                continue
+            value = self.rollout_policy(leaf, state.player())
+            self.backprop(leaf, value)
+        result = root.children.index(self.best_child(root, 0))
+        return root.child_actions[result]
+
+"""
+    **********************************************************************
+    CustomPlayer variable called by run_match.py (choose one of the above)
+    **********************************************************************
+"""
+
+CustomPlayer = MonteCarloUCTPlayer
